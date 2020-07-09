bba6491cb08c94bd2d84cde0d37fcef792bbf426
Tim Egenton <tje49@drexel.edu>
2020-07-09 16:13:33 -0400

    start using functions, prepare for getops support

--- mpdyt.sh
+++ mpdyt.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-case $# in
-0)
-	url=$(xclip -o)
-	case "$url" in
+function parseUrl() {
+	case "$1" in
 		*youtube.com/watch*) echo try again with playlist url ;;
 		*youtube.com/playlist*)
 			youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o '~/Music/%(uploader)s/%(playlist_title)s/%(playlist_index)d - %(title)s.%(ext)s' $url ;;
 		*.bandcamp.com/) wget -q -O - "$url" | grep -o -P "/album/\K[^\"]+" | while read album ; do youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o "~/Music/%(uploader)s/%(playlist_title)s/%(playlist_index)d - %(title)s.%(ext)s" "${url}/album/${album}"; done;;
-		*) echo "no valid url in clipboard" ;;
+		*) echo "invalid url" ;;
 	esac
+}
+
+OPTIONS=a:u:
+LONGOPTS=artist:,url:
+
+case $# in
+0)
+	url=$(xclip -o)
+	parseUrl "$url"
 	;;
 *)
 	echo 'bad args message'
