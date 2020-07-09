587ca8121d9d83fa9a6c707f83d300c1ba36867d
Tim Egenton <tje49@drexel.edu>
2020-07-09 17:56:33 -0400

    Now works with args and clipboard

--- mpdyt.sh
+++ mpdyt.sh
@@ -4,21 +4,40 @@ function parseUrl() {
 	case "$1" in
 		*youtube.com/watch*) echo try again with playlist url ;;
 		*youtube.com/playlist*)
-			youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o '~/Music/%(uploader)s/%(playlist_title)s/%(playlist_index)d - %(title)s.%(ext)s' $url ;;
-		*.bandcamp.com/) wget -q -O - "$url" | grep -o -P "/album/\K[^\"]+" | while read album ; do youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o "~/Music/%(uploader)s/%(playlist_title)s/%(playlist_index)d - %(title)s.%(ext)s" "${url}/album/${album}"; done;;
+			youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o '~/Music/%(uploader)s/%(playlist_title)s/%(playlist_index)d - %(title)s.%(ext)s' "$1" ;;
+		*.bandcamp.com/) wget -q -O - "$url" | grep -o -P "/album/\K[^\"]+" | while read album ; do youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o "~/Music/%(uploader)s/%(playlist_title)s/%(playlist_index)d - %(title)s.%(ext)s" "${1}/album/${album}"; done;;
 		*) echo "invalid url" ;;
 	esac
 }
 
 OPTIONS=a:u:
-LONGOPTS=artist:,url:
+LONGOPTS=artist:,urls:
 
-case $# in
-0)
-	url=$(xclip -o)
-	parseUrl "$url"
-	;;
-*)
-	echo 'bad args message'
-	;;
-esac
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+
+eval set -- "$PARSED"
+
+artist=- urlfile=-
+while true; do
+	case $1 in
+		-a|--artist)
+			artist="$2"
+			shift 2
+			;;
+		-u|--urls)
+			urlfile="$2"
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo 'bad args message'
+			;;
+	esac
+done
+
+[ $# -eq 1 ] && url="$1"
+
+[ ${url:-w} = "w" ] && parseUrl "$(xclip -o)" || parseUrl "$url"
