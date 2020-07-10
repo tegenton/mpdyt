647440b63c3468b0271220cfe72068fd812f3370
Tim Egenton <tje49@drexel.edu>
2020-07-09 21:40:20 -0400

    stdin and url file support

--- mpdyt.sh
+++ mpdyt.sh
@@ -7,7 +7,7 @@ function parseUrl() {
 		*youtube.com/playlist*)
 			youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o "$outputFormat" "$1" ;;
 		*.bandcamp.com/) wget -q -O - "$url" | grep -o -P "/album/\K[^\"]+" | while read album ; do youtube-dl --download-archive ~/Music/downloads.txt --add-metadata -x --audio-format flac -o "$outputFormat" "${1}/album/${album}"; done;;
-		*) echo "invalid url" ;;
+		*) echo "invalid url" ; return 1 ;;
 	esac
 }
 
@@ -18,7 +18,6 @@ PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 
 eval set -- "$PARSED"
 
-artist=- urlfile=-
 while true; do
 	case $1 in
 		-a|--artist)
@@ -39,6 +38,11 @@ while true; do
 	esac
 done
 
+[ "${urlfile:-w}" != "w" -o -t 0 -o "$url" = "-" ] || { while read line; do
+	[ ${line:-w} = "w" ]
+	parseUrl "$line"
+done < "${urlfile:-/dev/stdin}"; exit; }
+
 [ $# -eq 1 ] && url="$1"
 
-[ ${url:-w} = "w" ] && parseUrl "$(xclip -o)" || parseUrl "$url"
+[ "${url:-w}" = "w" ] && { parseUrl "$(xclip -o)"; exit; } || { parseUrl "$url"; exit; }
